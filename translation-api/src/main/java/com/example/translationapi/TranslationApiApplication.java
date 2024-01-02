11db1e496d59a5130c83470ebb7416e78f0cb53c
Jonathan94453 <cino.jonathan@gmail.com>
2024-01-02 14:58:19 -0600

    need to install maven

---
+++ translation-api/src/main/java/com/example/translationapi/TranslationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.translationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+
+@SpringBootApplication
+public class TranslationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranslationApiApplication.class, args); 
+	}
+
+}
