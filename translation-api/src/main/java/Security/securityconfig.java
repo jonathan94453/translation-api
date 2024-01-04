5363aeb663a53b6a499118ccd5cb369cc743eca7
Jonathan94453 <cino.jonathan@gmail.com>
2024-01-04 13:57:32 -0600

    Working error on a dependency springboot starter

--- translation-api/src/main/java/Security/securityconfig.java
+++ translation-api/src/main/java/Security/securityconfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,12 +15,14 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-public class securityconfig extends WebSecurityConfiguration {
-    private jwtAuthenticationFilter jwtAuthenticationFilter;
+public class securityconfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
+@Override
 protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable()
-                .authorizeRequests()
+   
+                
                     .antMatchers("/api/public").permitAll()
                     .antMatchers("/api/private").authenticated()
                 .and()
